03e1ae9a011915c91b9cd94ed2c45a3a471b20c8
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-24 16:17:35 -0400

    Update install.ps1

--- windowsdevinstall/install.ps1
+++ windowsdevinstall/install.ps1
@@ -6,3 +6,7 @@ C:\DEVwindows\python-3.10.0-amd64.exe
 rm C:\DEVwindows\python-3.10.0-amd64.exe
 wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/JavaSetup8u311.exe
 C:\DEVwindows\JavaSetup8u311.exe
+cd C:
+cd C:\DEVwindows
+wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/windevSDKscr/SDKcompile.ps1
+wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/windevSDKscr/SDKcompiler.py
