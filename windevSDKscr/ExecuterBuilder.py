b4aed158e9c28bd57e7c81152ee5ed6cb004d146
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-24 16:35:39 -0400

    Create ExecuterBuilder.py

---
+++ windevSDKscr/ExecuterBuilder.py
@@ -0,0 +1,9 @@
+my_file = open("Exelinker.config", "r")
+content_list = my_file. readlines()
+my_file.close()
+
+my_file = open("EXECUTE.ps1", "a)
+for x in content_list:
+    file1.write(content_list)
+
+    
