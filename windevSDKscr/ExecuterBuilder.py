1f3043fdd788a74aa369c87881cd9e96ced22e54
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-24 17:58:48 -0400

    Update ExecuterBuilder.py

--- windevSDKscr/ExecuterBuilder.py
+++ windevSDKscr/ExecuterBuilder.py
@@ -2,7 +2,7 @@ my_file = open("Exelinker.config", "r")
 content_list = my_file. readlines()
 my_file.close()
 
-my_file = open("EXECUTE.ps1", "a)
+my_file = open("EXECUTE.ps1", "w")
 for x in content_list:
     file1.write(content_list)
 
