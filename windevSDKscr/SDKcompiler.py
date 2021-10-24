185bdca25e45e8957e8c3f8d64623bf5967516dc
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-24 16:14:33 -0400

    Create SDKcompiler.py

---
+++ windevSDKscr/SDKcompiler.py
@@ -0,0 +1,25 @@
+file1 = open('compiler.config', 'r')
+Lines = file1.readlines()
+ 
+count = 0
+for line in Lines:
+    count += 1
+    if count == 1:
+      name = line
+    if count == 2:
+      powershellcompile = line
+file1.close()
+
+
+my_file = open("PScompiler.config", "r")
+content_list = my_file. readlines()
+my_file.close()
+    
+
+file1 = open("cpile.ps1", "a")  # append mode
+file1.write("jar -cf " + name + " *.java\n")
+file1.write("wget STICKSCLIENT\n")
+if powershellcompile == "TruePS1":
+  for x in content_list:
+      file1.write("ps2exe " + x + ".ps1")
+file1.close()
