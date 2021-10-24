2685549a2b4cbfa9de6a55577f3c34e2a10193a7
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-24 12:50:12 -0400

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,8 @@
+Install-Module PS2EXE
+mkdir C:\DEVwindows
+cd C:\DEVwindows
+wget https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
+C:\DEVwindows\python-3.10.0-amd64.exe
+rm C:\DEVwindows\python-3.10.0-amd64.exe
+wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/JavaSetup8u311.exe
+C:\DEVwindows\JavaSetup8u311.exe
