1e9e726531a7d778210e9f03cdfea16239356d53
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-24 17:43:01 -0400

    Update install.ps1

--- windowsdevinstall/install.ps1
+++ windowsdevinstall/install.ps1
@@ -1,13 +1,6 @@
 Install-Module PS2EXE
 mkdir C:\DEVwindows
 cd C:\DEVwindows
-wget https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
-C:\DEVwindows\python-3.10.0-amd64.exe
-rm C:\DEVwindows\python-3.10.0-amd64.exe
-wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/JavaSetup8u311.exe
-C:\DEVwindows\JavaSetup8u311.exe
-cd C:
-cd C:\DEVwindows
 wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/windevSDKscr/SDKcompile.ps1
 wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/windevSDKscr/SDKcompiler.py
 wget https://github.com/ArdenyUser/SticksExpiramentalProgramLibary/raw/main/windevSDKscr/ExecuterBuilder.py
