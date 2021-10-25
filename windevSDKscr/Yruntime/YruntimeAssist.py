b728dcce5a962ec69ce40c02d328f25c1b206b23
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-25 19:12:43 -0400

    Update and rename YruntimeCompiler.py to YruntimeAssist.py

---
+++ windevSDKscr/Yruntime/YruntimeAssist.py
@@ -0,0 +1,5 @@
+def broadcast(aug):
+	f = open("com.y", "w")
+	f.write(aug)
+	f.close()
+	
--- windevSDKscr/Yruntime/YruntimeCompiler.py
+++
@@ -1,7 +0,0 @@
-file1 = open('project.ypak', 'r')
-Lines = file1.readlines()
-
-count = 0
-for line in Lines:
-	count += 1
-	
