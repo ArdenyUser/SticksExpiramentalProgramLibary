ae6c84bd0a4e32bb991ae350c80977ca218f4905
ArdenyUser <71571434+ArdenyUser@users.noreply.github.com>
2021-10-26 16:42:49 -0400

    Update vasobuilder.py

--- windevSDKscr/VasoBeta/vasobuilder.py
+++ windevSDKscr/VasoBeta/vasobuilder.py
@@ -3,7 +3,25 @@ content_list = my_file. readlines()
 my_file.close()
 True
 x = 0
+allowcmd = 1
 while True:
     x = x + 1
-    
+    file = open(content_list[x], "r")
+
+    for line in file:
+        for character in line:
+            previous = character
+            m = 1
+            while m = 1:
+               if != 9:
+                    try:
+                        parser = character + previous
+                    previous = character
+                    if allowcmd == 1:
+                        if parser == "print":
+                            allowcmd = 9
+               else:
+                    for character in line:
+                        parser = character + previous
+                        previous = character
     
